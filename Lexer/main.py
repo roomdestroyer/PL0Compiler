9de9bc7f7b262f4ff719cb52b7ba4fae5bc88ead
JinZhou5042 <jzhou24@nd.edu>
2023-10-01 23:19:03 -0400

    update

--- Lexer/main.py
+++ Lexer/main.py
@@ -5,7 +5,8 @@ from utils import lexer_input_dir, lexer_output_dir
 
 def main():
     for file in os.listdir(lexer_input_dir):
-        with open(lexer_input_dir + "./" + file, "r") as input_file:
+        # with open(lexer_input_dir + "./" + file, "r") as input_file:
+        with open(os.path.join(lexer_input_dir, file), "r") as input_file:
             output_file = lexer_output_dir + "./" + file.split(".")[0] + ".out"
             if os.path.exists(output_file):
                 os.remove(output_file)
